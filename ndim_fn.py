a85808114c516bf9048d358b2377b427e9d3ef43
balon <cooba.balon@gmail.com>
2026-06-16 11:14:27 +0200

    implemented 5 ndim functions for ae and de assignment

--- ndim_fn.py
+++ ndim_fn.py
@@ -1,11 +1,42 @@
 import jax
 import jax.numpy as jnp
 
+# funkcje tłumaczone z matlabowych implementacji, przeanalizowane i zwektoryzowane gdzie się dało, przygotowane do akceleracji
 
 # https://www.sfu.ca/~ssurjano/Code/ackleym.html
+@jax.jit(static_argnames = ['a', 'b', 'c'])
 def ackley(xs: jnp.ndarray, a = 20, b = 0.2, c = jnp.pi * 2):
     sum1 = jnp.sum(xs ** 2)
-    sum2 = jnp.sum(jnp.cos(c) * xs)
+    sum2 = jnp.sum(jnp.cos(c * xs))
     term1 = -a * jnp.exp(-b * jnp.sqrt(sum1 / xs.shape[0]))
     term2 = - jnp.exp(sum2 / xs.shape[0])
-    return term1 + term2 + a + jnp.exp(1)
+    return term1 + term2 + a + jnp.e
+
+# https://www.sfu.ca/~ssurjano/Code/michalm.html
+@jax.jit(static_argnames = 'm')
+def michalewicz(xs: jnp.ndarray, m = 10):
+    return -jnp.sum(jnp.sin(xs) * (jnp.sin(jnp.arange(1, xs.shape[0] + 1) * xs ** 2 / jnp.pi)) ** (2 * m)) # arange zastępuje tutaj zależność od pozycji w pętli z oryginalnej implementacji; gdyby zużycie pamięci było krytyczne można by tutaj użyć jax.lax.scan i lecieć w pętli jak w oryginale, żeby uniknąć materializacji tego sztucznego wektora indeksów w pamięci
+
+# https://www.sfu.ca/~ssurjano/Code/rastrm.html
+@jax.jit()
+def rastrigin(xs: jnp.ndarray):
+    return 10 * xs.shape[0] + jnp.sum(xs ** 2 - 10 * jnp.cos(2 * jnp.pi * xs))
+
+# https://www.sfu.ca/~ssurjano/langer.html
+@jax.jit()
+def langermann(
+        xs: jnp.ndarray, 
+        # pomijam tutaj argument m = 5 z oryginału, bo wydaje mi się, że służy tylko sprawdzeniu kształtu wektora c oraz pierwszego wymiaru macierzy A; zakładam, że w matlabowej wersji była to kwestia implementacyjna związana ze specyfiką języka, a nie istotna część funkcji
+        c = jnp.array([1, 2, 5, 2, 3]),
+        A = jnp.array([[3, 5], [5, 2], [2, 1], [1, 4], [7, 9]])
+    ):
+    # pomijam sprawdzenie zgodności kształtu, jax ma swoje tracebacki od tego
+    inner = jnp.sum((xs[None, :] - A) ** 2, axis=1) # tutaj działa auto broadcast, analogicznie do numpy
+    return jnp.sum(c * jnp.exp(-inner / jnp.pi) * jnp.cos(jnp.pi * inner)) # outer
+
+# https://www.sfu.ca/~ssurjano/Code/griewankm.html
+@jax.jit()
+def griewank(xs: jnp.ndarray):
+    sum = jnp.sum(xs ** 2 / 4000)
+    prod = jnp.prod(jnp.cos(xs / jnp.sqrt(jnp.arange(1, xs.shape[0] + 1)))) # ta sama akcja z indeksami co w funkcji Michalewicza w 18 linii kodu
+    return sum - prod + 1
