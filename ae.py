d624109660e2b481c88bd3c19108092cae2efa96
balon <cooba.balon@gmail.com>
2026-06-16 15:40:19 +0200

    first round of improvements

--- ae.py
+++ ae.py
@@ -1,5 +1,3 @@
-import time
-
 import jax
 import jax.numpy as jnp
 
@@ -8,6 +6,7 @@ from typing import Callable, Tuple
 def evolutionary_algorithm(
         objective: Callable,
         domain: Tuple[float, float],
+        n_dim: int = 2,
         population_size: int = 100,
         generations: int = 100,
         mutation_std: float = 0.05,
@@ -17,11 +16,11 @@ def evolutionary_algorithm(
     mutation_std = mutation_std * (domain[1] - domain[0])
 
     init_key = jax.random.key(seed)
-    init_population = jax.random.uniform(init_key, (population_size,), minval = domain[0], maxval = domain[1])
-    init_fitness = jax.vmap(objective)(population_size)
-    init_best = jnp.argmin[init_fitness] # mniej = lepiej
-    init_worst = jnp.argmax[init_fitness]
-    init_mean = jnp.mean[init_fitness]
+    init_population = jax.random.uniform(init_key, (population_size, n_dim), minval = domain[0], maxval = domain[1])
+    init_fitness = jax.vmap(objective)(init_population)
+    init_best = jnp.argmin(init_fitness) # mniej = lepiej
+    init_worst = jnp.argmax(init_fitness)
+    init_mean = jnp.mean(init_fitness)
 
     state = {
         "key": init_key,
@@ -37,7 +36,7 @@ def evolutionary_algorithm(
         key = state["key"]
         population = state["population"]
         fitness = state["fitness"]
-        key, parent_key, pairing_key, crossover_key, mutation_key = jax.random.split(key, 6)
+        key, parent_key, pairing_key, crossover_key, mutation_key = jax.random.split(key, 5)
 
         # wybór rodziców
         parent_key_map = jax.random.split(parent_key, population_size)
@@ -45,18 +44,19 @@ def evolutionary_algorithm(
         @jax.vmap
         def get_parent(key): # takie coś wymyśliłem, żeby nie było powtórek w puli kandydatów (chociaż pewnie z powtórkami też by działało, a kod byłby prostszy)
             pool = jax.random.permutation(key, population_size)[:parent_selection_pool_size]
-            best =  jnp.argmin(fitness[pool])
-            return population[best]
+            best_idx_pool =  jnp.argmin(fitness[pool])
+            best_idx = pool[best_idx_pool]
+            return population[best_idx]
         
         parents = get_parent(parent_key_map)
         
         # krzyżowanie
         pairs = jax.random.permutation(pairing_key, parents)
-        alpha = jax.random.uniform(crossover_key, (population_size,))
+        alpha = jax.random.uniform(crossover_key, (population_size, 1))
         children = alpha * parents + (1.0 - alpha) * pairs
         
         # mutacja
-        children = children + mutation_std * jax.random.normal(mutation_key, children.shape)
+        children = children + mutation_std * jax.random.normal(mutation_key, population_size)
         children = jnp.clip(children, domain[0], domain[1])
 
         # selekcja
@@ -88,6 +88,8 @@ def evolutionary_algorithm(
         state = step(state)
         results.append(state)
         print(f'POKOLENIE{generation}')
-        print(f'NAJLEPSZY:{state["best"]:.2f};;NAJGORSZY:{state["worst"]:.2f};;ŚREDNIA:{state["mean"]:.2f}')
+        print(f'NAJLEPSZY:{state["fitness"][state["best"]]} ({state["population"][state["best"]]})')
+        print(f'NAJGORSZY:{state["fitness"][state["worst"]]} ({state["population"][state["worst"]]})')
+        print(f'ŚREDNIA:{state["mean"]:.2f}')
 
     return results
