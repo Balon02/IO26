56782d7dd7a258d5e0bccff9a1bc8874a7bead8d
balon <cooba.balon@gmail.com>
2026-06-16 12:26:57 +0200

    added domain vars (min, max) for the implemented functions

--- ndim_fn.py
+++ ndim_fn.py
@@ -12,15 +12,21 @@ def ackley(xs: jnp.ndarray, a = 20, b = 0.2, c = jnp.pi * 2):
     term2 = - jnp.exp(sum2 / xs.shape[0])
     return term1 + term2 + a + jnp.e
 
+ackley_domain = (-32.768, 32.768)
+
 # https://www.sfu.ca/~ssurjano/Code/michalm.html
 @jax.jit(static_argnames = 'm')
 def michalewicz(xs: jnp.ndarray, m = 10):
     return -jnp.sum(jnp.sin(xs) * (jnp.sin(jnp.arange(1, xs.shape[0] + 1) * xs ** 2 / jnp.pi)) ** (2 * m)) # arange zastępuje tutaj zależność od pozycji w pętli z oryginalnej implementacji; gdyby zużycie pamięci było krytyczne można by tutaj użyć jax.lax.scan i lecieć w pętli jak w oryginale, żeby uniknąć materializacji tego sztucznego wektora indeksów w pamięci
 
+michalewicz_domain = (0.0, jnp.pi)
+
 # https://www.sfu.ca/~ssurjano/Code/rastrm.html
 @jax.jit()
 def rastrigin(xs: jnp.ndarray):
     return 10 * xs.shape[0] + jnp.sum(xs ** 2 - 10 * jnp.cos(2 * jnp.pi * xs))
+
+rastrigin_domain = (-5.12, 5.12)
 
 # https://www.sfu.ca/~ssurjano/langer.html
 @jax.jit()
@@ -34,9 +40,13 @@ def langermann(
     inner = jnp.sum((xs[None, :] - A) ** 2, axis=1) # tutaj działa auto broadcast, analogicznie do numpy
     return jnp.sum(c * jnp.exp(-inner / jnp.pi) * jnp.cos(jnp.pi * inner)) # outer
 
+langermann_domain = (0.0, 10.0)
+
 # https://www.sfu.ca/~ssurjano/Code/griewankm.html
 @jax.jit()
 def griewank(xs: jnp.ndarray):
     sum = jnp.sum(xs ** 2 / 4000)
     prod = jnp.prod(jnp.cos(xs / jnp.sqrt(jnp.arange(1, xs.shape[0] + 1)))) # ta sama akcja z indeksami co w funkcji Michalewicza w 18 linii kodu
     return sum - prod + 1
+
+griewank_domain = (-600.0, 600.0)
