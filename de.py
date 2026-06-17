broadcast fix in de

de.py
@@ -55,7 +55,7 @@ def differential_evolution(
         ensure_mutation_mask = jnp.greater(jnp.sum(crossover_mask, axis=1), 0) # część populacji u której żadne mutacje nie przeszły
         forced_mutation_idxs = jax.random.randint(forced_mutation_key, (population_size,), minval = 0, maxval = n_dim) # wylosowanie po 1 indeksie do mutacji 'na wszelki wypadek' dla każdego osobnika
         forced_mutation_mask = jnp.zeros_like(crossover_mask).at[jnp.arange(population_size), forced_mutation_idxs].set(1) # pełna maska tych 'na wszelkich wypadków'
-        crossover_mask = jnp.where(ensure_mutation_mask, crossover_mask, forced_mutation_mask) # merge masek z wymuszonym wyborem tylko tam, gdzie jest potrzebny
+        crossover_mask = jnp.where(ensure_mutation_mask[:, None], crossover_mask, forced_mutation_mask) # merge masek z wymuszonym wyborem tylko tam, gdzie jest potrzebny
             # chyba to trochę przekombinowałem, ale w ten sposób odsetek wyboru cech mutant vs oryginał będzie bliższy crossover_prob niż gdybyśmy 'na chama' nałożyli i jedną i drugą maskę
 
         trials = jnp.where(crossover_mask, mutants, population) # nałożenie maski